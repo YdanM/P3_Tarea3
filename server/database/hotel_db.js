634ed7aa55a16d4e467479ad094ec75ce24fffc2
YdanM <162137586+YdanM@users.noreply.github.com>
2025-11-24 22:14:53 -0400

    Agrega carpeta a feature/database

---
+++ server/database/hotel_db.js
@@ -0,0 +1,33 @@
+
+//Diccionario
+
+export const restaurante = {
+    Ember : {
+        capacidad : 3,
+        horarios: {
+            '6 - 8' : [], //lista
+            '8 - 10' : [] //lista
+        }
+    },
+    Zao : {
+        capacidad : 4,
+        horarios : {
+            '6 - 8' : [],
+            '8 - 10' : []
+        }
+    },
+    Grappa : {
+        capacidad : 3,
+        horarios : {
+            '6 - 8' : [],
+            '8 - 10' : []
+        }
+    },
+    Larimar : {
+        capacidad : 2,
+        horarios : {
+            '6 - 8' : [],
+            '8 - 10' : []
+        }
+    }
+}
